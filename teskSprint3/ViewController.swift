28b11b14f84892bbdebb1beb6aa75501302f3174
Alyona Epishina <pach2105@yandex.ru>
2023-03-23 17:46:56 +0300

    Написан код для работы счетчика

--- teskSprint3/ViewController.swift
+++ teskSprint3/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var counter: Int = 0
 
+    @IBOutlet weak var textLable: UILabel!
+    
+    @IBOutlet weak var tapButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tapButton.tintColor = .purple
     }
 
-
+    
+    @IBAction func buttonDidTap(_ sender: Any) {
+        counter += 1
+        textLable.text = "Значение счетчика: \(counter)"
+    }
 }
 
